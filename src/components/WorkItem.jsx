a8e310c329587585c7547cb434ea371a1543bbbc
Javier Chavez <javier120699lili@gmail.com>
2021-01-31 20:40:59 -0500

    Adding slides effects to a work

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -1,14 +1,16 @@
 function WorkItem({ work }) {
     return (
-        <div className="work__item">
-            <img className="work__image" src={work.img} alt="" />
-            <article className="work__details">
-                <header className="work__header">
-                    <span className="work__subtitle">Mi trabajo</span>
-                    <h3 className="work__title">{work.name}</h3>
-                </header>
-                <p className="work__description">{work.description}</p>
-            </article>
+        <div className="work__container">
+            <div className="work__item">
+                <img className="work__image" src={work.img} alt="" />
+                <article className="work__details">
+                    <header className="work__header">
+                        <span className="work__subtitle">Mi trabajo</span>
+                        <h3 className="work__title">{work.name}</h3>
+                    </header>
+                    <p className="work__description">{work.description}</p>
+                </article>
+            </div>
         </div>
     )
 }
--- src/views/Work.jsx
+++ src/views/Work.jsx
@@ -1,28 +1,97 @@
 import "../assets/css/Work.css"
 import "../assets/css/View.css"
 
+import React from "react"
 import WorkItem from "../components/WorkItem"
 
-function Work() {
-    const works = [
-        {
-            img: "./assets/img/work/personal-page.png",
-            name: "Personal page",
-            description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repellat hic ut dolorem quidem fugit, doloremque repudiandae
-            ea harum in. Commodi officia quibusdam eius recusandae omnis
-            sunt aliquid, a accusantium perferendis.`,
-        },
-    ]
+import { TimelineMax, TweenMax } from "gsap"
+import ScrollMagic from "scrollmagic"
+import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap"
 
-    return (
-        <section id="work" className="work view">
-            <h2 className="subtitle">Mi trabajo</h2>
+ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax)
 
-            {works.map((work) => (
-                <WorkItem key={work.name} work={work} />
-            ))}
-        </section>
-    )
+const works = [
+    {
+        img: "./assets/img/work/personal-page.png",
+        name: "Personal page",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repellat hic ut dolorem quidem fugit, doloremque repudiandae
+        ea harum in. Commodi officia quibusdam eius recusandae omnis
+        sunt aliquid, a accusantium perferendis.`,
+    },
+    {
+        img: "./assets/img/work/personal-page.png",
+        name: "Trabajo",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repellat hic ut dolorem quidem fugit, doloremque repudiandae
+        ea harum in. Commodi officia quibusdam eius recusandae omnis
+        sunt aliquid, a accusantium perferendis.`,
+    },
+    {
+        img: "./assets/img/work/personal-page.png",
+        name: "Trabajo 2",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repellat hic ut dolorem quidem fugit, doloremque repudiandae
+        ea harum in. Commodi officia quibusdam eius recusandae omnis
+        sunt aliquid, a accusantium perferendis.`,
+    },
+    {
+        img: "./assets/img/work/personal-page.png",
+        name: "Trabajo 3",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repellat hic ut dolorem quidem fugit, doloremque repudiandae
+        ea harum in. Commodi officia quibusdam eius recusandae omnis
+        sunt aliquid, a accusantium perferendis.`,
+    },
+    {
+        img: "./assets/img/work/personal-page.png",
+        name: "Trabajo 4",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repellat hic ut dolorem quidem fugit, doloremque repudiandae
+        ea harum in. Commodi officia quibusdam eius recusandae omnis
+        sunt aliquid, a accusantium perferendis.`,
+    },
+]
+class Work extends React.Component {
+    componentDidMount() {
+        const controller = new ScrollMagic.Controller()
+        const movePercent = 100 / works.length
+        const wipeAnimation = new TimelineMax()
+        for (let index = 1; index < works.length; index++) {
+            wipeAnimation
+                .to("#worksContainer", 0.5, { z: -150 }) // move back in 3D space
+                .to("#worksContainer", 1, { x: `-${index * movePercent}%` }) // move in to first panel
+                .to("#worksContainer", 0.5, { z: 0 }) // move back to origin in 3D space
+        }
+
+        // create scene to pin and link animation
+        new ScrollMagic.Scene({
+            triggerElement: "#work",
+            triggerHook: "onLeave",
+            duration: "500%",
+        })
+            .setPin("#work")
+            .setTween(wipeAnimation)
+            .addTo(controller)
+    }
+    render() {
+        return (
+            <section id="work" className="work view">
+                <h2 className="subtitle">Mi trabajo</h2>
+
+                <div className="works">
+                    <div
+                        id="worksContainer"
+                        style={{ width: works.length + "00%" }}
+                        className="works__container"
+                    >
+                        {works.map((work) => (
+                            <WorkItem key={work.name} work={work} />
+                        ))}
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
 export default Work
