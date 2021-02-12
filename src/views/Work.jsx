ce0962797c5f9d042a715e2720cd6c327de32e46
Javier Chavez <javier120699lili@gmail.com>
2021-02-12 00:44:55 -0500

    Adding work content

--- src/views/Work.jsx
+++ src/views/Work.jsx
@@ -9,36 +9,37 @@ import ScrollMagic from "scrollmagic"
 
 const works = [
     {
+        img: "./assets/img/work/inbox.png",
+        name: "Inbox",
+        description: `Inbox es una plataforma omnicanal, que facilita la comunicación entre empresas y sus clientes. 
+        Actualmente pertenezco al equipo de Inbox, y estamos convirtiéndolo en una plataforma automantenible.
+         El stack con el que trabajo en Inbox es Vue.js, Adonis.js, MongoDB, Socket.io y Express.js.`,
+    },
+    {
         img: "./assets/img/work/personal-page.png",
         name: "Personal page",
-        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Repellat hic ut dolorem quidem fugit, doloremque repudiandae
-        ea harum in. Commodi officia quibusdam eius recusandae omnis
-        sunt aliquid, a accusantium perferendis.`,
+        description: `Mi página personal es un proyecto que ansiaba poner en marcha, y aún está en desarrollo. 
+        Aquí es donde puedo mostrar mis principales trabajos y describir mis habilidades. 
+        Realicé esta página usando React por la gran curiosidad que tenía de probar esta librería y agregarla a mi portafolio.`,
     },
     {
-        img: "./assets/img/work/personal-page.png",
-        name: "Trabajo",
-        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Repellat hic ut dolorem quidem fugit, doloremque repudiandae
-        ea harum in. Commodi officia quibusdam eius recusandae omnis
-        sunt aliquid, a accusantium perferendis.`,
+        img: "./assets/img/work/buscapatas.png",
+        name: "Buscapatas",
+        description: `Buscapatas es una plataforma de adopción y donación para el banco de alimentos y mascotas del Perú. 
+        Lideré el equipo de backend, en donde desarrollamos una API Rest usando servicios de AWS, principalmente funciones Lambda en JavaScript, APIGateway y S3.`,
     },
     {
-        img: "./assets/img/work/personal-page.png",
-        name: "Trabajo 2",
-        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Repellat hic ut dolorem quidem fugit, doloremque repudiandae
-        ea harum in. Commodi officia quibusdam eius recusandae omnis
-        sunt aliquid, a accusantium perferendis.`,
+        img: "./assets/img/work/gerencia-gestar.png",
+        name: "GerenciaGestar",
+        description: `GerenciaGestar es una aplicación gerencial para Invitro gestar, que consume datos de su ERP para poder obtener reportes gerenciales. 
+        Me encargué del backend de esta aplicación trabajando con vanilla PHP.`,
     },
     {
-        img: "./assets/img/work/personal-page.png",
-        name: "Trabajo 3",
-        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Repellat hic ut dolorem quidem fugit, doloremque repudiandae
-        ea harum in. Commodi officia quibusdam eius recusandae omnis
-        sunt aliquid, a accusantium perferendis.`,
+        img: "./assets/img/work/securitec-music.png",
+        name: "SecuritecMusic",
+        description: `Securitec music es una API Rest que administra Artistas, albums y canciones. 
+        Fue realizado como una prueba de ingreso al equipo de Inbox, en la cual puede demostrar mis habilidades como backend developer. 
+        Para realizar esta utilicé Express.js y MySql, basandome en una arquitectura hexagonal. `,
     },
 ]
 class Work extends React.Component {
